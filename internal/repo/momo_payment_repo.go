0fb9feb2ffce846dd800bb2bfa3ede5ea42e4e32
Võ Nguyên Gia Bảo <21127017@student.hcmus.edu.vn>
2024-10-08 21:17:58 +0700

    added interface for repo

--- internal/repo/momo_payment_repo.go
+++ internal/repo/momo_payment_repo.go
@@ -1,1 +1,14 @@
 package repo
+
+type MoMoRepo interface {
+	CreatePayment(orderID, amount string) (string, error)
+	CheckPaymentStatus(orderID string) (bool, error)
+	RefundPayment(orderID, amount string) (string, error)
+}
+
+type momoRepo struct {
+	endpoint    string
+	partnerCode string
+	accessKey   string
+	secrectKey  string
+}
