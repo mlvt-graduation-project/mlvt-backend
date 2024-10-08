a61e077517e9abf8e7d53faa5a1d9a5590b6a8f6
Võ Nguyên Gia Bảo <21127017@student.hcmus.edu.vn>
2024-10-08 21:25:17 +0700

    service: added interface

--- internal/service/momo_payment_service.go
+++ internal/service/momo_payment_service.go
@@ -1,1 +1,7 @@
 package service
+
+type PaymentService interface {
+	GeneratePaymentQRCode(orderID, amount string) ([]byte, error)
+	CheckPaymentStatus(orderID string) (bool, error)
+	RefundPayment(orderID, amount string) (string, error)
+}
