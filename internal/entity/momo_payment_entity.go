c3ed0d1b9153c8aa9e9d5fbfe542e09b70ec2a6d
Võ Nguyên Gia Bảo <21127017@student.hcmus.edu.vn>
2024-10-08 21:12:11 +0700

    added New method and generates signature with HMAC SHA256

--- internal/entity/momo_payment_entity.go
+++ internal/entity/momo_payment_entity.go
@@ -1,5 +1,11 @@
 package entity
 
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+)
+
 type MoMoRequest struct {
 	PartnerCode string
 	AccessKey   string
@@ -7,4 +13,23 @@ type MoMoRequest struct {
 	Amount      string
 	OrderID     string
 	Signature   string
+}
+
+// NewMoMoRequest is a factory function to create a MoMoRequest with the necessary fields
+func NewMoMoRequest(partnerCode, accessKey, requestID, amount, orderID string) *MoMoRequest {
+	return &MoMoRequest{
+		PartnerCode: partnerCode,
+		AccessKey:   accessKey,
+		RequestID:   requestID,
+		Amount:      amount,
+		OrderID:     orderID,
+	}
+}
+
+// GenerateSignature generates an HMAC SHA256 signature for the MoMo payment
+func (r *MoMoRequest) GenerateSignature(secrectKey string) {
+	rawSignature := r.PartnerCode + r.RequestID + r.Amount + r.AccessKey
+	h := hmac.New(sha256.New, []byte(secrectKey))
+	h.Write([]byte(rawSignature))
+	r.Signature = hex.EncodeToString(h.Sum(nil))
 }
