98ef1b895b15c1ec87ee987f6e5e2405f4e0b6a1
Võ Nguyên Gia Bảo <21127017@student.hcmus.edu.vn>
2024-10-13 10:43:04 +0700

    added video status

--- internal/entity/video_entity.go
+++ internal/entity/video_entity.go
@@ -2,15 +2,26 @@ package entity
 
 import "time"
 
+// Define the status type with limited string options
+type VideoStatus string
+
+const (
+	StatusRaw        VideoStatus = "raw"
+	StatusProcessing VideoStatus = "processing"
+	StatusFailed     VideoStatus = "failed"
+	StatusSuccess    VideoStatus = "success"
+)
+
 type Video struct {
-	ID          uint64    `json:"id"`
-	Title       string    `json:"title"`
-	Duration    int       `json:"duration"` // Duration of the video in seconds
-	Description string    `json:"description"`
-	FileName    string    `json:"file_name"` //
-	Folder      string    `json:"folder"`
-	Image       string    `json:"image"`
-	UserID      uint64    `json:"user_id"`    // ID of the user who uploaded the video
-	CreatedAt   time.Time `json:"created_at"` // Timestamp of when the video was created
-	UpdatedAt   time.Time `json:"updated_at"` // Timestamp of the last update to the video
+	ID          uint64      `json:"id"`
+	Title       string      `json:"title"`
+	Duration    int         `json:"duration"` // Duration of the video in seconds
+	Description string      `json:"description"`
+	FileName    string      `json:"file_name"` //
+	Folder      string      `json:"folder"`
+	Image       string      `json:"image"`
+	Status      VideoStatus `json:"status"`
+	UserID      uint64      `json:"user_id"`    // ID of the user who uploaded the video
+	CreatedAt   time.Time   `json:"created_at"` // Timestamp of when the video was created
+	UpdatedAt   time.Time   `json:"updated_at"` // Timestamp of the last update to the video
 }
