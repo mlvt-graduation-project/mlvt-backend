6dedcec49e47a626a4aebefd9f8909012ceb10c3
capigiba <21127017@student.hcmus.edu.vn>
2025-03-28 09:32:47 +0700

    feat(entity): add wallet transaction entity

---
+++ internal/entity/wallet_transaction_entity.go
@@ -0,0 +1,19 @@
+package entity
+
+import "time"
+
+type WalletTransactionType string
+
+const (
+	TransactionTypeDeposit  WalletTransactionType = "deposit"
+	TransactionTypeWithdraw WalletTransactionType = "withdraw"
+)
+
+type WalletTransaction struct {
+	ID          uint64                `json:"id"`
+	UserID      uint64                `json:"user_id"`
+	Type        WalletTransactionType `json:"type"`         // deposit or withdraw
+	Amount      uint64                `json:"amount"`       // token-based amount
+	VoucherCode *string               `json:"voucher_code"` // optional voucher reference
+	CreatedAt   time.Time             `json:"created_at"`
+}
