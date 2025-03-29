a4e0e9e1417f3a5cb446d1307218088e10d139a0
capigiba <21127017@student.hcmus.edu.vn>
2025-03-29 16:18:55 +0700

    feat(voucher-code): init structure

---
+++ internal/entity/voucher_entity.go
@@ -0,0 +1,18 @@
+package entity
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type VoucherCode struct {
+	Id          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Code        string             `json:"code" bson:"code"`
+	Token       uint64             `json:"token" bson:"token"`
+	MaxUsage    uint               `json:"max_usage" bson:"max_usage"`
+	UsedCount   uint               `json:"used_count" bson:"used_count"`
+	ExpiredTime time.Time          `json:"expired_time" bson:"expired_time"`
+	CreatedAt   time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt   time.Time          `json:"updated_at" bson:"updated_at"`
+}
