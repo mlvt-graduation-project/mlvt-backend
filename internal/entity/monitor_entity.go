f0c2c5b8054265b46630e689d0067bfc57723271
capigiba <21127017@student.hcmus.edu.vn>
2025-04-01 16:53:42 +0700

    feat(admin-monitor): declare entity

---
+++ internal/entity/monitor_entity.go
@@ -0,0 +1,20 @@
+package entity
+
+type MonitorMetric struct {
+	Count     uint64 `json:"count" bson:"count"`
+	Succeeded uint64 `json:"succeeded" bson:"succeeded"`
+	Failed    uint64 `json:"failed" bson:"failed"`
+}
+
+type MonitorPipeline struct {
+	TTS MonitorMetric `json:"tts" bson:"tts"`
+	TTT MonitorMetric `json:"ttt" bson:"ttt"`
+	STT MonitorMetric `json:"stt" bson:"stt"`
+	LS  MonitorMetric `json:"ls" bson:"ls"`
+}
+
+type MonitorDataType struct {
+	Videos MonitorMetric `json:"videos" bson:"videos"`
+	Audios MonitorMetric `json:"audios" bson:"audios"`
+	Texts  MonitorMetric `json:"texts" bson:"texts"`
+}
